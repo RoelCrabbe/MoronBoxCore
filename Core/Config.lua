e06fca9db52041ca39ee6e826259bc45c271ad5c
RoelCrabbe <crabberoel@gmail.com>
2025-09-03 17:50:33 +0200

    revert back to Xlimidrizer, ppor drogles boost

--- Core/Config.lua
+++ Core/Config.lua
@@ -62,7 +62,7 @@ MB_raidAssist = { -- Raid tools
 	Priest = { -- Priest options
 		PowerInfusion = { -- Players in this list can receive Power Infusion (randomly selected)
 			["Liket"] = { "Alionex" },
-			["Blaidzy"] = { "Drogles" },
+			["Blaidzy"] = { "Xlimidrizer" },
 			["Cyal"] = { "Oxg" },
 			["Bonita"] = { "Drogles" }
 		}
