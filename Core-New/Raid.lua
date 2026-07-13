022d92e8d50eadbb8290d150589ab919dffe1f1c
RoelCrabbe <crabberoel@gmail.com>
2026-07-14 01:30:00 +0200

    next thing is the CORE GetTarget

--- Core-New/Raid.lua
+++ Core-New/Raid.lua
@@ -440,78 +440,6 @@ function Raid.GetTargetNotOnTank()
     end
 end
 
--- [[ GTFO ]] --
-
-function Raid.GTFO()
-    if not MB_raidAssist.GTFO.Active then
-        return
-    end
-
-    mb_useSandsOnChromaggus()
-
-    if mb_imFocus() then
-        return
-    end
-
-    if Instance.ONY() and MB_myOnyxiaBoxStrategy then
-        if Raid.TankTarget("Onyxia") and (Raid.TankTargetHealth() <= 0.65 and Raid.TankTargetHealth() >= 0.4) and myName ~= MB_myOnyxiaMainTank then
-            if Raid.FocusAggro() then
-                if myClass == "Paladin" and mb_spellReady("Divine Shield") then
-                    CastSpellByName("Divine Shield")
-                    return
-                end
-
-                local runTank = Unit.ReturnPlayerInRaidFromTable(MB_raidAssist.GTFO.Onyxia)
-                local runTankId = MoronBox.Core.State.MBID[runTank]
-
-                if runTankId and Unit.IsAlive(runTankId) then
-                    FollowByName(runTank, 1)
-                end
-            else
-                local mainTankId = MoronBox.Core.State.MBID[MB_myOnyxiaFollowTarget]
-
-                if mainTankId and Unit.InRange(mainTankId) then
-                    if not Unit.InMeleeRange(mainTankId) then
-                        FollowByName(MB_myOnyxiaFollowTarget, 1)
-                    end
-                end
-            end
-        end
-    end
-
-    if not Unit.AggroOnPlayer() then
-        if Instance.NAXX() then
-            GLUTH_GetOUT()
-            GROB_GetOUT()
-            mb_useFirePotsOnFaerlina()
-        elseif Instance.BWL() and mb_hasBuffOrDebuff("Burning Adrenaline", "player", "debuff") then
-            if myClass == "Paladin" and mb_spellReady("Divine Shield") then
-                CastSpellByName("Divine Shield")
-                return
-            end
-
-            local vaelTank = Unit.ReturnPlayerInRaidFromTable(MB_raidAssist.GTFO.Vaelastrasz)
-            local vaelTankId = MoronBox.Core.State.MBID[vaelTank]
-
-            if vaelTankId and Unit.IsAlive(vaelTankId) then
-                FollowByName(vaelTank, 1)
-            end
-        elseif Instance.MC() and mb_hasBuffOrDebuff("Living Bomb", "player", "debuff") then
-            if myClass == "Paladin" and mb_spellReady("Divine Shield") then
-                CastSpellByName("Divine Shield")
-                return
-            end
-
-            local baronTank = Unit.ReturnPlayerInRaidFromTable(MB_raidAssist.GTFO.Baron)
-            local baronTankId = MoronBox.Core.State.MBID[baronTank]
-
-            if baronTankId and Unit.IsAlive(baronTankId) then
-                FollowByName(baronTank, 1)
-            end
-        end
-    end
-end
-
 -- [[ Auto Mark Moam ]] --
 
 local function IsMoamOrManaFiend(targetName)
@@ -629,4 +557,76 @@ end
 
 function Raid.IsAtRazorgore()
     return GetSubZoneText() == "Dragonmaw Garrison"
+end
+
+-- [[ GTFO ]] --
+
+function Raid.GTFO()
+    if not MB_raidAssist.GTFO.Active then
+        return
+    end
+
+    mb_useSandsOnChromaggus()
+
+    if mb_imFocus() then
+        return
+    end
+
+    if Instance.ONY() and MB_myOnyxiaBoxStrategy then
+        if Raid.TankTarget("Onyxia") and (Raid.TankTargetHealth() <= 0.65 and Raid.TankTargetHealth() >= 0.4) and myName ~= MB_myOnyxiaMainTank then
+            if Raid.FocusAggro() then
+                if myClass == "Paladin" and mb_spellReady("Divine Shield") then
+                    CastSpellByName("Divine Shield")
+                    return
+                end
+
+                local runTank = Unit.ReturnPlayerInRaidFromTable(MB_raidAssist.GTFO.Onyxia)
+                local runTankId = MoronBox.Core.State.MBID[runTank]
+
+                if runTankId and Unit.IsAlive(runTankId) then
+                    FollowByName(runTank, 1)
+                end
+            else
+                local mainTankId = MoronBox.Core.State.MBID[MB_myOnyxiaFollowTarget]
+
+                if mainTankId and Unit.InRange(mainTankId) then
+                    if not Unit.InMeleeRange(mainTankId) then
+                        FollowByName(MB_myOnyxiaFollowTarget, 1)
+                    end
+                end
+            end
+        end
+    end
+
+    if not Unit.AggroOnPlayer() then
+        if Instance.NAXX() then
+            GLUTH_GetOUT()
+            GROB_GetOUT()
+            mb_useFirePotsOnFaerlina()
+        elseif Instance.BWL() and mb_hasBuffOrDebuff("Burning Adrenaline", "player", "debuff") then
+            if myClass == "Paladin" and mb_spellReady("Divine Shield") then
+                CastSpellByName("Divine Shield")
+                return
+            end
+
+            local vaelTank = Unit.ReturnPlayerInRaidFromTable(MB_raidAssist.GTFO.Vaelastrasz)
+            local vaelTankId = MoronBox.Core.State.MBID[vaelTank]
+
+            if vaelTankId and Unit.IsAlive(vaelTankId) then
+                FollowByName(vaelTank, 1)
+            end
+        elseif Instance.MC() and mb_hasBuffOrDebuff("Living Bomb", "player", "debuff") then
+            if myClass == "Paladin" and mb_spellReady("Divine Shield") then
+                CastSpellByName("Divine Shield")
+                return
+            end
+
+            local baronTank = Unit.ReturnPlayerInRaidFromTable(MB_raidAssist.GTFO.Baron)
+            local baronTankId = MoronBox.Core.State.MBID[baronTank]
+
+            if baronTankId and Unit.IsAlive(baronTankId) then
+                FollowByName(baronTank, 1)
+            end
+        end
+    end
 end
